8fa707801896fc6ce17c43223f69ec28f56f228e
Nathan Alday <n.c.alday@gmail.com>
2011-06-30 21:54:21 -0500

    Fixed error with build_book.sh script.

--- scripts/compile_book.sh
+++ scripts/compile_book.sh
@@ -5,6 +5,6 @@ cp $PYTHONFORSMELOCATION/book/*.tex $PYTHONFORSMELOCATION/book/tmp
 cp $PYTHONFORSMELOCATION/book/*.bib $PYTHONFORSMELOCATION/book/tmp
 cd $PYTHONFORSMELOCATION/book/tmp
 
-sh compile_tex.sh AnIntroductionToPythonForScienceMathAndEngineering
+sh $PYTHONFORSMELOCATION/scripts/compile_tex.sh AnIntroductionToPythonForScienceMathAndEngineering
 
 mv $PYTHONFORSMELOCATION/book/tmp/AnIntroductionToPythonForScienceMathAndEngineering.pdf $PYTHONFORSMELOCATION
